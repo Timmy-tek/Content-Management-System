7a882cb5e9be1ae014257597d7172797c963bc31
Adegbola Timothy <adegbolatimothyjosh@gmail.com>
2026-09-08 09:07:03 +0100

    Fixed Linkedin connection bug

--- src/app/settings/accounts/page.tsx
+++ src/app/settings/accounts/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useApp } from '@/context/AppContext';
 import { Platform } from '@/types';
 import { PlatformBadge } from '@/components/PlatformBadge';
